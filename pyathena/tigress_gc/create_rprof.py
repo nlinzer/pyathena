0e106e1cfd9c446eaf73a51694064c6bd65611b4
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-08-14 15:46:06 +0900

    minor change

--- pyathena/tigress_gc/create_rprof.py
+++ pyathena/tigress_gc/create_rprof.py
@@ -8,44 +8,41 @@ import xarray as xr
 from pyathena.classic.cooling import coolftn
 from pyathena.util.wmean import wmean
 
+Thot1, Twarm, Tcold = 2.e4, 5050, 184.
+
 def _transform_to_Rz(dat, Redges):
     """
     Caveat: Scale heights need a special treatment here.
     """
-    # update phase information
-    cf = coolftn()
-    u = Units()
-    pok = dat['pressure']*u.pok
-    T1 = pok/(dat['density']*u.muH)
-    dat['temperature'] = xr.DataArray(cf.get_temp(T1.values), coords=T1.coords, dims=T1.dims)
-    Thot1, Twarm, Tcold = 2.e4, 5050, 184.
     warm = (dat['temperature'] > Twarm)&(dat['temperature'] < Thot1)
     unstable = (dat['temperature'] > Tcold)&(dat['temperature'] < Twarm)
     cold = dat['temperature'] < Tcold
 
     Nr = len(Redges)-1
     dat['R'] = np.sqrt(dat.x**2+dat.y**2)
+    dat['zsq'] = dat.z**2
     newdat = []
     Rbins = 0.5*(Redges[1:]+Redges[:-1])
     for i in range(Nr):
         Rl, Rr = Redges[i], Redges[i+1]
         mask = (Rl < dat.R)&(dat.R < Rr)
 
-        arr = (dat.z**2).where(mask)
-        notnull = arr.notnull()
-        H = np.sqrt(wmean(arr, dat['density'].where(notnull), dim=['x','y','z']))
+        # total
+        arr = dat.zsq.where(mask)
+        H = np.sqrt(wmean(arr, dat['density'].where(arr.notnull()), dim=['x','y','z']))
 
-        arr = (dat.z**2).where(warm&mask)
-        notnull = arr.notnull()
-        Hw = np.sqrt(wmean(arr, dat['density'].where(notnull), dim=['x','y','z']))
+        # warm
+        arr = dat.zsq.where(warm&mask)
+        Hw = np.sqrt(wmean(arr, dat['density'].where(arr.notnull()), dim=['x','y','z']))
 
-        arr = (dat.z**2).where(unstable&mask)
-        notnull = arr.notnull()
-        Hu = np.sqrt(wmean(arr, dat['density'].where(notnull), dim=['x','y','z']))
+        # unstable
+        arr = dat.zsq.where(unstable&mask)
+        Hu = np.sqrt(wmean(arr, dat['density'].where(arr.notnull()), dim=['x','y','z']))
 
-        arr = (dat.z**2).where(cold&mask)
-        notnull = arr.notnull()
-        Hc = np.sqrt(wmean(arr, dat['density'].where(notnull), dim=['x','y','z']))
+        # cold
+        arr = dat.zsq.where(cold&mask)
+        Hc = np.sqrt(wmean(arr, dat['density'].where(arr.notnull()), dim=['x','y','z']))
+
 
         datRz = dat.where(mask).mean(dim=['x','y']).expand_dims(dim={'R':Rbins[i:i+1]})
         datRz = datRz.assign(H=H.expand_dims('R'))
@@ -67,6 +64,17 @@ def create_rprof(s, num):
     ds = s.load_vtk(num=num)
     dat = ds.get_field(field=['density','velocity','pressure'], as_xarray=True)
 
+    # update phase information
+    cf = coolftn()
+    u = Units()
+    pok = dat['pressure']*u.pok
+    T1 = pok/(dat['density']*u.muH)
+    dat['temperature'] = xr.DataArray(cf.get_temp(T1.values), coords=T1.coords, dims=T1.dims)
+    warm = (dat['temperature'] > Twarm)&(dat['temperature'] < Thot1)
+    unstable = (dat['temperature'] > Tcold)&(dat['temperature'] < Twarm)
+    cold = dat['temperature'] < Tcold
+    twophase = dat['temperature'] < Thot1
+
     # transform from (x,y,z) to (R,z) coordinates (azimuthal average)
     Rmin, Rmax, dR = 0, 256, 10
     Redges = np.arange(Rmin, Rmax, dR)
@@ -74,7 +82,7 @@ def create_rprof(s, num):
     dat = _transform_to_Rz(dat, Redges)
 
     # add fields
-    dat['Sigma_gas'] = ((dat['density']*ds.domain['dx'][axis_idx['z']]).sum(dim='z')*s.u.Msun/s.u.pc**2)
+    dat['Sigma_gas'] = (dat['density']*ds.domain['dx'][axis_idx['z']]).sum(dim='z')*s.u.Msun/s.u.pc**2
     dat['Pth'] = dat['pressure'].interp(z=0)
     dat['Pturb'] = (dat['density']*dat['velocity3']**2).interp(z=0)
 
