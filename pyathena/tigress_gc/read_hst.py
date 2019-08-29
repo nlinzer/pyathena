6f2d99a942a63573e8f1146b1fd072a3a6b2383a
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-08-30 00:03:25 +0900

    update read_hst: add mass flux through xyz boundaries; add ejecta mass

--- pyathena/tigress_gc/read_hst.py
+++ pyathena/tigress_gc/read_hst.py
@@ -30,7 +30,7 @@ class ReadHst:
         Ly = domain['Lx'][1]
         Lz = domain['Lx'][2]
         # area of domain
-        area = [Lx*Ly, Lz*Lx, Ly*Lz]
+        area = [Ly*Lz, Lz*Lx, Lx*Ly]
 
         # Orbital time at the bulge scale length rb
         rb = 120
@@ -57,21 +57,45 @@ class ReadHst:
         h['msp'] = hst['msp']*vol*u.Msun
         h['msp_left'] = hst['msp_left']*vol*u.Msun
 
-        # Total outflow mass
-        for i, direction in enumerate(['F3','F2','F1']):
-            flux = (hst[direction+'_upper'] - hst[direction+'_lower'])\
-                    *area[i]*u.mass_flux*u.length**2
-            h['mass_out'] = integrate.cumtrapz(flux, h['time'], initial=0.0)
-
-        # Calculate (cumulative) SN ejecta mass
-        # JKIM: only from clustered type II(?)
-        try:
-            sn = read_hst(self.files['sn'], force_override=force_override)
-            t_ = np.array(hst['time'])
-            Nsn, snbin = np.histogram(sn.time, bins=np.concatenate(([t_[0]], t_)))
-            h['mass_snej'] = Nsn.cumsum()*self.par['feedback']['MejII'] # Mass of SN ejecta [Msun]
-        except:
+        if self.par['problem']['iflw_flag']==1: #constant inflow rate
+            # Total inflow mass
+            Mdot = (2*self.par['problem']['iflw_d0']*self.par['problem']['iflw_v0']\
+                     *self.par['problem']['iflw_mu']*self.par['problem']['iflw_w']\
+                     *self.par['problem']['iflw_h']*self.u.density*self.u.velocity\
+                     *self.u.length**2).to("Msun/Myr").value
+            h['mass_in'] = Mdot*h['time']
+        else:
+            #TODO time varying inflow
             pass
+
+        # Total outflow mass
+        h['mass_out1'] = 0
+        h['mass_out2'] = 0
+        h['mass_out3'] = 0
+        for i, direction in enumerate(['F1','F2','F3']):
+            flux = ((hst[direction+'_upper'] - hst[direction+'_lower']).to_numpy()
+                    *area[i]*u.mass_flux*u.length**2).to("Msun/Myr").value
+            h['flux'+str(i+1)] = flux
+            h['mass_out'+str(i+1)] += integrate.cumtrapz(flux, h['time'], initial=0.0)
+        h['mass_out2'] += h['mass_in']
+
+        # Total outflow mass
+        h['mass_out'] = 0
+        for i, direction in enumerate(['F1','F2','F3']):
+            flux = ((hst[direction+'_upper'] - hst[direction+'_lower']).to_numpy()
+                    *area[i]*u.mass_flux*u.length**2).to("Msun/Myr").value
+            h['mass_out'] += integrate.cumtrapz(flux, h['time'], initial=0.0)
+        h['mass_out'] += h['mass_in']
+
+#        # Calculate (cumulative) SN ejecta mass
+#        # JKIM: only from clustered type II(?)
+#        try:
+#            sn = read_hst(self.files['sn'], force_override=force_override)
+#            t_ = np.array(hst['time'])
+#            Nsn, snbin = np.histogram(sn.time, bins=np.concatenate(([t_[0]], t_)))
+#            h['mass_snej'] = Nsn.cumsum()*self.par['feedback']['MejII'] # Mass of SN ejecta [Msun]
+#        except:
+#            pass
 
         # star formation rates [Msun/yr]
         h['sfr1'] = hst['sfr1']*(Lx*Ly/1e6)
